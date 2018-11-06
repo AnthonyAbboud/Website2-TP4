28bf0ae020c0e7690f54c2ff365e7283c1b63702
Anthony <anthony.abboud@polymtl.ca>
2018-11-06 17:36:03 -0500

    Setup initial + setup mongoDB user: log4420user pass: log4420 (npm test now functional)

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,32 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
 });
 
+router.get("/index.html", (req, res) => {
+  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+});
+
+router.get("/products.html", (req, res) => {
+  res.render("products", { title: "Produits", message: "Ça semble fonctionner!" });
+});
+
+router.get("/product.html", (req, res) => {
+  res.render("product", { title: "Produit", message: "Ça semble fonctionner!" });
+});
+
+router.get("/contact.html", (req, res) => {
+  res.render("contact", { title: "Contact", message: "Ça semble fonctionner!" });
+});
+
+router.get("/shopping-cart.html", (req, res) => {
+  res.render("shopping-cart", { title: "Panier", message: "Ça semble fonctionner!" });
+});
+
+router.get("/order.html", (req, res) => {
+  res.render("order", { title: "Commande", message: "Ça semble fonctionner!" });
+});
+
+router.get("/confirmation.html", (req, res) => {
+  res.render("confirmation", { title: "Confirmation", message: "Ça semble fonctionner!" });
+});
+
 module.exports = router;
