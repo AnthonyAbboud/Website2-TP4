bd6f68060fa6a17aaf36c7bf453521a526f2c91f
sidy <sidy.fall@groupe-esigelec.org>
2018-11-13 19:47:10 -0500

    shopping cart en cours

--- assets/scripts/shopping-cart.controller.js
+++ assets/scripts/shopping-cart.controller.js
@@ -15,13 +15,14 @@ var onlineShop = onlineShop || {};
    * @private
    */
   function _updateCount() {
-    const itemsCount = shoppingCartService.getItemsCount();
-    const countElement = $(".shopping-cart").find(".count");
-    if (itemsCount > 0) {
-      countElement.addClass("visible").text(itemsCount);
-    } else {
-      countElement.removeClass("visible");
-    }
+    shoppingCartService.getItemsCount().done(itemsCount => {
+      const countElement = $(".shopping-cart").find(".count");
+      if (itemsCount > 0) {
+        countElement.addClass("visible").text(itemsCount);
+      } else {
+        countElement.removeClass("visible");
+      }
+    });
   }
 
   /**
@@ -44,112 +45,72 @@ var onlineShop = onlineShop || {};
     $("#shopping-cart-container").html("<p>Aucun produit dans le panier.</p>");
   }
 
-  /**
-   * Renders the view when the shopping cart isn't empty.
-   *
-   * @param items   The items in the shopping cart.
-   * @private
-   */
-  function _renderShoppingCartView(items) {
-    const shoppingCartTable = $(".shopping-cart-table tbody");
-    items.forEach(item => {
-      const product = item.product;
-      const rowElement = _createItemElement(item);
+  // Initializes the "add to cart" form.
+  $("#add-to-cart-form").submit(e => {
+    e.preventDefault();
+    const productId = +$(e.target).attr("data-product-id");
+    shoppingCartService.addItem(productId, +$(e.target).find("input").val()).done(() => {
+      const dialog = $("#dialog");
+      dialog.fadeIn();
+      setTimeout(() => dialog.fadeOut(), 5000);
 
-      // Updates the quantity for a specific item and update the view.
-      function updateQuantity(quantity) {
-        rowElement.find(".remove-quantity-button").prop("disabled", quantity <= 1);
-        shoppingCartService.updateItemQuantity(product.id, quantity);
+      _updateCount();
+      shoppingCartService.getItemQuantity(productId).done(quantity => $("#shopping-cart-quantity").text(quantity));
+    });
+  });
 
-        _updateCount();
-        _updateTotalAmount();
-        rowElement.find(".quantity").text(quantity);
-        rowElement.find(".price").html(utils.formatPrice(product["price"] * quantity));
-      }
+  // Initializes the shopping cart table.
+  $(".shopping-cart-table > tbody >  tr").each(function() {
+    const rowElement = $(this);
+    const productId = +rowElement.attr("data-product-id");
 
-      rowElement.find(".remove-item-button").click(() => {
-        if (confirm("Voulez-vous supprimer le produit du panier?")) {
-          shoppingCartService.removeItem(product.id);
-          rowElement.remove();
-          if (shoppingCartService.getItemsCount() === 0) {
+    // Updates the quantity for a specific item and update the view.
+    function updateQuantity(quantity) {
+      rowElement.find(".remove-quantity-button").prop("disabled", quantity <= 1);
+      shoppingCartService.updateItemQuantity(productId, quantity).done(() => {
+		_updateCount();
+		_updateTotalAmount();
+	  });
+      rowElement.find(".quantity").text(quantity);
+      shoppingCartService.getItem(productId).done(item => {
+        rowElement.find(".price").html(utils.formatPrice(item.product["price"] * quantity));
+      });
+    }
+
+    rowElement.find(".remove-item-button").click(() => {
+      if (confirm("Voulez-vous supprimer le produit du panier?")) {
+        shoppingCartService.removeItem(productId);
+        rowElement.remove();
+        shoppingCartService.getItemsCount().done(itemsCount => {
+          if (itemsCount === 0) {
             _renderEmptyView();
           } else {
             _updateTotalAmount();
           }
-          _updateCount();
-        }
-      });
-      rowElement.find(".remove-quantity-button").click(() => {
-        updateQuantity(shoppingCartService.getItemQuantity(product.id) - 1);
-      });
-      rowElement.find(".add-quantity-button").click(() => {
-        updateQuantity(shoppingCartService.getItemQuantity(product.id) + 1);
-      });
-
-      shoppingCartTable.append(rowElement);
-    });
-  }
-
-  /**
-   * Creates an item element.
-   *
-   * @param item                The item to use to create the element.
-   * @returns {*|HTMLElement}   A jQuery element.
-   * @private
-   */
-  function _createItemElement(item) {
-    return $(`<tr>
-      <td><button class="remove-item-button" title="Supprimer"><i class="fa fa-times"></i></button></td>
-      <td><a href="./product.html?id=${item.product.id}">${item.product.name}</a></td>
-      <td>${utils.formatPrice(item.product["price"])}</td>
-      <td>
-      <div class="row">
-      <div class="col"><button class="remove-quantity-button" title="Retirer" 
-        ${(item.quantity <= 1) ? "disabled" : ""}>
-      <i class="fa fa-minus"></i></button></div>
-      <div class="col quantity">${item.quantity}</div>
-      <div class="col"><button class="add-quantity-button" title="Ajouter"><i class="fa fa-plus"></i></button></div>
-      </div>
-      </td>
-      <td class="price">${utils.formatPrice(item.total)}</td>
-      </tr>`);
-  }
-
-  // Initializes the shopping cart.
-  $("#add-to-cart-form").submit(event => {
-    event.preventDefault();
-    const productId = $(event.target).attr("data-product-id");
-    shoppingCartService.addItem(productId, +$(event.target).find("input").val());
-
-    const dialog = $("#dialog");
-    dialog.fadeIn();
-    setTimeout(function() {
-      dialog.fadeOut();
-    }, 5000);
-
-    _updateCount();
-    $("#shopping-cart-quantity").text(shoppingCartService.getItemQuantity(productId));
-  });
-
-  $("#remove-all-items-button").click(() => {
-    if (confirm("Voulez-vous supprimer tous les produits du panier?")) {
-      shoppingCartService.removeAllItems();
-      _renderEmptyView();
-      _updateCount();
-    }
-  });
-
-  // Checks if we are on the "shopping cart" page.
-  if ($("#shopping-cart-container").length) {
-    shoppingCartService.getItems().done(items => {
-      if (items.length === 0) {
-        _renderEmptyView();
-      } else {
-        _renderShoppingCartView(items);
+        });
+        _updateCount();
       }
     });
-    _updateTotalAmount();
-  }
-  _updateCount();
+    rowElement.find(".remove-quantity-button").click(() => {
+      shoppingCartService.getItemQuantity(productId).done(quantity => {
+        updateQuantity(quantity - 1);
+      });
+    });
+    rowElement.find(".add-quantity-button").click(() => {
+      shoppingCartService.getItemQuantity(productId).done(quantity => {
+        updateQuantity(quantity + 1);
+      });
+    });
+  });
+
+  // Initializes the "remove all items" button.
+  $("#remove-all-items-button").click(() => {
+    if (confirm("Voulez-vous supprimer tous les produits du panier?")) {
+      shoppingCartService.removeAllItems().done(() => {
+		_renderEmptyView();
+		_updateCount();   
+	  });
+    }
+  });
 
 })(jQuery, onlineShop.shoppingCartService, onlineShop.utils);
